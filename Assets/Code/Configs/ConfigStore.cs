3e2264f08607e2ccea95c24123b4d96ee8e8b00e
Дон <dsvinkabot@gmail.com>
2021-11-25 23:47:03 +0300

    Добавлена конфигурации локаций

--- Assets/Code/Configs/ConfigStore.cs
+++ Assets/Code/Configs/ConfigStore.cs
@@ -10,15 +10,18 @@ namespace Code.Configs
         [SerializeField] [AssetPath.Attribute(typeof(HudConfig))] private string _hudCfgPath;
         [SerializeField] [AssetPath.Attribute(typeof(BulletConfig))] private string _bulletCfgPath;
         [SerializeField] [AssetPath.Attribute(typeof(SpriteAnimatorConfig))] private string _coinAnimCfgPath;
+        [SerializeField] [AssetPath.Attribute(typeof(LevelConfig))] private string _levelCfgPath;
 
         private HudConfig _hudCfg;
         private PlayerConfig _playerCfg;
         private BulletConfig _bulletCfg;
         private SpriteAnimatorConfig _coinAnimCfg;
+        private LevelConfig _levelCfg;
         
         public PlayerConfig PlayerCfg => GetData(_playerCfgPath, _playerCfg);
         public HudConfig HudCfg => GetData(_hudCfgPath, _hudCfg);
         public BulletConfig BulletCfg => GetData(_bulletCfgPath, _bulletCfg);
         public SpriteAnimatorConfig CoinAnimCfg => GetData(_coinAnimCfgPath, _coinAnimCfg);
+        public LevelConfig LevelCfg => GetData(_levelCfgPath, _levelCfg);
     }
 }
---
+++ Assets/Code/Configs/LevelConfig.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using static Code.Utils.DataUtils;
+
+namespace Code.Configs
+{
+    [CreateAssetMenu(fileName = "LevelCfg", menuName = "Configs/Level Cfg", order = 1)]
+    internal sealed class LevelConfig : ScriptableObject
+    {
+        [SerializeField] [AssetPath.Attribute(typeof(GameObject))] 
+        private string _prefabPath;
+
+        [SerializeField] private Tile _groundTile;
+        [SerializeField] private int _mapWidth;
+        [SerializeField] private int _mapHeight;
+        [SerializeField] private bool _borders;
+
+        [SerializeField] [Range(0, 100)] private int _fillPercent;
+        [SerializeField] [Range(0, 100)] private int _smoothFactor;
+
+        private GameObject _prefab;
+
+        public GameObject Prefab => GetData(_prefabPath, _prefab);
+        public Tile GroundTile => _groundTile;
+        public int MapWidth => _mapWidth;
+        public int MapHeight => _mapHeight;
+        public bool Borders => _borders;
+        
+        public int FillPercent => _fillPercent;
+        public int SmoothFactor => _smoothFactor;
+    }
+}
